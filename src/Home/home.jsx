0d49ab562ec8187d0778e190370ab008073cbb0b
Enrique Ramirez <enrique.ramirez@oracle.com>
2022-03-08 22:27:36 -0700

    added basic pages and routing

---
+++ src/Home/home.jsx
@@ -0,0 +1,83 @@
+import './home.css'
+function HomeScreen() { 
+
+  return (
+    <div className="home">
+      <header>
+        <h3>Workout Diary</h3>
+        <nav className="navigation">
+          <ul>
+            <li>Home</li>
+            <li>Library</li>
+            <li>Quickstart</li>
+            <li>Community</li>
+            <li>Profile</li>
+          </ul>
+        </nav>
+      </header>
+
+      <section className="welcome-section">
+        <h1 className="welcome-message">Welcome Dude</h1>
+      </section>
+
+      <section className="history-sections">
+        <ul className="workouts">
+          <li className="todays-workout card">
+            <h3>Todays workout</h3>
+            <p>
+              <strong>Duration: </strong> 60 minutes
+            </p>
+            <p>
+              <strong>Exercises:</strong>
+              <ul>
+                <li>
+                  <p>Turkish getups</p>
+                  <p>Reps: 5</p>
+                  <p>Sets: 5</p>
+                  <p>Weight: 16kg</p>
+                  <p>Work capacity (sets x reps x weight): 400kg </p>
+                </li>
+                <li>
+                  <p>Swings</p>
+                  <p>Reps: 5</p>
+                  <p>Sets: 5</p>
+                  <p>Weight: 16kg</p>
+                  <p>Work capacity (sets x reps x weight): 400kg </p>
+                </li>
+              </ul>
+            </p>
+          </li>
+
+          <li className="workout card">
+            <h3>Last workout</h3>
+            <p>
+              <strong>Duration: </strong> 60 minutes
+            </p>
+            <p>
+              <strong>Exercises:</strong>
+              <ul>
+                <li>
+                  <p>Turkish getups</p>
+                  <p>Reps: 5</p>
+                  <p>Sets: 5</p>
+                  <p>Weight: 16kg</p>
+                  <p>Work capacity (sets x reps x weight): 400kg </p>
+                </li>
+                <li>
+                  <p>Swings</p>
+                  <p>Reps: 5</p>
+                  <p>Sets: 5</p>
+                  <p>Weight: 16kg</p>
+                  <p>Work capacity (sets x reps x weight): 400kg </p>
+                </li>
+              </ul>
+            </p>
+          </li>
+        </ul>
+      </section>
+    </div>
+  );
+
+}
+
+export default HomeScreen;
---
+++ src/Library/Library.jsx
@@ -0,0 +1,10 @@
+function Library() { 
+  return (
+    <div>
+      <h2>Library section</h2>
+      <p>This is the library</p>
+    </div>
+  )
+}
+
+export default Library
